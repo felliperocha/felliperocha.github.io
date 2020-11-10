3c1dc8e6b77eae68cf5fd519a7f4993963febb9c
Fellipe Rocha <empreendedorismoatual@gmail.com>
2020-11-10 16:04:05 -0300

    Delete Treino Markdown.mk

--- Treino Markdown.mk
+++
@@ -1,8 +0,0 @@
-#Sucesso no Marketing Digital
-
-##Tudo que você precisa saber para ter sucesso nesse mercado.
-
-Saiba agora todos os truques e artimanhas dos principais marketeiros digitais do mundo.
-
-Você vai aprender neste curso os segredos guardados a sete chaves para você conseguir lucrar muito pela internet.
-
